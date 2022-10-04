da17e1f4edec48296682a6dc63e01c4e05071eed
Krhystofferson Ferrari <102234632+KrhystFerrari@users.noreply.github.com>
2022-10-04 19:30:26 -0300

    A propriedade 'key'

--- react-section3/src/components/ListRender.js
+++ react-section3/src/components/ListRender.js
@@ -3,11 +3,22 @@ import { useState } from "react";
 const ListRender = () => {
     const [list] = useState(["João", "Pedro", "José"]);
 
+    const [users] = useState([
+        {id: 1, name: "Krhys", age: 30},
+        {id: 3243262, name: "João", age: 28},
+        {id: 564789, name: "Pedro", age: 18},
+    ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name} - {user.age}</li>
             ))}
         </ul>
     </div>
