2d938e24eadedf4741a4498e2eae0d84fab811ec
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-26 19:58:53 -0300

    update aula43

--- react-section3/src/App.js
+++ react-section3/src/App.js
@@ -8,6 +8,8 @@ import ManageData from './components/ManageData';
 import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
 import Fragments from './components/Fragments';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
 
 function App() {
   //const name = 'Joaquim';
@@ -18,6 +20,10 @@ function App() {
     {id: 2, brand: "KIA", color: "Branco", newCar: false, km: 34343},
     {id: 3, brand: "Renault", color: "Azul", newCar: false, km: 234},
   ];
+
+  function showMessage() {
+    console.log('Evento do componente pai!')
+  }
 
   return (
     <div className="App">
@@ -43,6 +49,7 @@ function App() {
       {/* loop em array de objetos */}
       {cars.map((car) => (
         <CarDetails 
+          key={car.id}
           brand={car.brand} 
           color={car.color} 
           km={car.km} 
@@ -51,6 +58,15 @@ function App() {
       ))};
       {/* fragment */}
       <Fragments propFragment='teste' />
+      {/* children */}
+      <Container myValue='teste1'>
+        <p>E este é o conteudo</p>
+      </Container>
+      <Container myValue='teste2'>
+        <h5>Testando o container</h5>
+      </Container>
+      {/* executar função */}
+      <ExecuteFunction myFunction={showMessage} />
     </div>
   );
 }
